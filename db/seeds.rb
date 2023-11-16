6753bf793c91b8d38bc030f6f9ffe8716c274742
Jerome PRINCET <jerome@princet.fr>
2023-11-16 12:33:41 +0100

    Ajout Seed via API

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
+# URL de l'API
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+
+# Ouvrir l'URL et lire la réponse
+response = URI.open(url).read
+
+# Parser la réponse JSON
+movies = JSON.parse(response)['results']
+
+# Boucler sur les films et les créer dans la base de données
+movies.each do |movie|
+  # Ici, adaptez les champs selon votre modèle de Movie
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts 'Movies created successfully!'
